90febbf1e03634f0fc20396eadab75756caaf11e
SaurabhPowar <108181410+Saurabh-Powar@users.noreply.github.com>
2025-05-03 22:58:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+    base: './', // Ensures relative paths for assets
+  build: {
+    outDir: 'public', // Output directory for production build
+  },
+
 })
