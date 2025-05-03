6fa2b8c8fbf839581bbfa7cc7c32d978754b896d
SaurabhPowar <108181410+Saurabh-Powar@users.noreply.github.com>
2025-05-03 22:07:45 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,14 +2,8 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  server: {
-    port: "https://employee-management-system-d658.onrender.com", // Change this if necessary
-  },
   plugins: [react()],
   server: {
-    port: 3000,
-    proxy: {
-      "/api": "http://localhost:3000",
     },
   },
 })
