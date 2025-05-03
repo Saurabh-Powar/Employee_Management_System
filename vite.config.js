aba70180a59182ef1331977fd52b531afc40645a
SaurabhPowar <108181410+Saurabh-Powar@users.noreply.github.com>
2025-05-04 01:23:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,11 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
   base: './', // Ensures relative paths for assets
-  root: 'public', // Directory containing index.html
   build: {
     outDir: 'dist' // Output directory
   },
   server: {
-    port: 5000, // Customize port if needed
+    port: 3000, // Customize port if needed
     open: true, // Automatically open the app in the browser
-  }
+  },
 })
