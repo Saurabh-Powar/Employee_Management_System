ed5ae0260451d763640696f6e024841ee8a5c638
SaurabhPowar <108181410+Saurabh-Powar@users.noreply.github.com>
2025-05-04 18:41:10 +0530

    Update socket.js

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,27 +1,27 @@
 // Example of how to implement Socket.io on your client side
 // Add this to a new file like client/src/services/socket.js
 
-import { io } from "socket.io-client";
+import { io } from "socket.io-client"
 
 const socket = io("https://employee-management-system-gv8r.onrender.com", {
   withCredentials: true,
-  autoConnect: false
-});
+  autoConnect: false,
+})
 
 export const connectSocket = (userId) => {
   if (userId) {
-    socket.auth = { userId };
-    socket.connect();
+    socket.auth = { userId }
+    socket.connect()
   }
-};
+}
 
 export const disconnectSocket = () => {
-  socket.disconnect();
-};
+  socket.disconnect()
+}
 
 export const subscribeToNotifications = (callback) => {
-  socket.on("notification", callback);
-  return () => socket.off("notification", callback);
-};
+  socket.on("notification", callback)
+  return () => socket.off("notification", callback)
+}
 
-export default socket;
+export default socket
