dda8a98f7873a93ad4401d782ccc91c9362847be
SaurabhPowar <108181410+Saurabh-Powar@users.noreply.github.com>
2025-05-04 14:29:35 +0530

    Create socket.js

---
+++ client/src/services/socket.js
@@ -0,0 +1,27 @@
+// Example of how to implement Socket.io on your client side
+// Add this to a new file like client/src/services/socket.js
+
+import { io } from "socket.io-client";
+
+const socket = io("https://employee-management-system-d658.onrender.com", {
+  withCredentials: true,
+  autoConnect: false
+});
+
+export const connectSocket = (userId) => {
+  if (userId) {
+    socket.auth = { userId };
+    socket.connect();
+  }
+};
+
+export const disconnectSocket = () => {
+  socket.disconnect();
+};
+
+export const subscribeToNotifications = (callback) => {
+  socket.on("notification", callback);
+  return () => socket.off("notification", callback);
+};
+
+export default socket;
